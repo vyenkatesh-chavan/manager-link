c540220fecb241890a2838cadeec3d1157d266e1
vyenkatesh-chavan <vyankateshc21@gmail.com>
2026-08-07 12:06:32 +0530

    Save local changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
+  baseURL: "https://link-manager-webapp.onrender.com",
   withCredentials: true, // Send and receive cookies
   headers: {
     "Content-Type": "application/json",
