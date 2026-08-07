7004b3b943ea963cb29e590e296a1112479db7b7
Vyenkatesh Chavan <162705902+vyenkatesh-chavan@users.noreply.github.com>
2026-08-07 12:39:00 +0530

    Update API base URL for production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
-  baseURL: "https://link-manager-webapp.onrender.com",
+  baseURL: "https://manager-link-ic17.onrender.com",
   withCredentials: true, // Send and receive cookies
   headers: {
     "Content-Type": "application/json",
