a4dd0df39b992cfd4bcf82975c5dcfabe5e540f6
Vyenkatesh Chavan <162705902+vyenkatesh-chavan@users.noreply.github.com>
2026-08-07 12:49:02 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+ 
   baseURL: "https://manager-link-ic17.onrender.com",
   withCredentials: true, // Send and receive cookies
   headers: {
